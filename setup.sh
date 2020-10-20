3c92aeeb6eb668e52c33aa8632fde218ad6b3784
felt1s <kostbg1@gmail.com>
2020-10-20 19:39:48 +0300

    All work

--- setup.sh
+++ setup.sh
@@ -7,6 +7,9 @@ docker build -t nginx:latest srcs/nginx/;
 docker build -t mysql:latest srcs/mysql/;
 docker build -t wordpress:latest srcs/wordpress/;
 docker build -t phpmyadmin:latest srcs/phpmyadmin/;
+docker build -t influxdb srcs/influxdb;
+docker build -t telegraf srcs/telegraf;
+docker build -t grafana srcs/grafana;
 docker build -t ftps srcs/ftps;
 kubectl create -f srcs/yaml/
 minikube dashboard;
