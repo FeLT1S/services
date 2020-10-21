7d94f114b214a54d49a3a19d16ffeb3a46a88cb0
felt1s <kostbg1@gmail.com>
2020-10-21 18:01:46 +0300

    small fixes

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 minikube start --driver=virtualbox --memory='3000' --disk-size 5000MB;
 minikube addons enable metallb;
 minikube addons enable dashboard;
-kubectl apply -f srcs/yaml/metallb.yaml;
+kubectl apply -f srcs/metallb.yaml;
 eval $(minikube docker-env);
 docker build -t nginx:latest srcs/nginx/;
 docker build -t mysql:latest srcs/mysql/;
